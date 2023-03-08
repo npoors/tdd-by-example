4aedbbc46bdc7d82c2a61bf3d1f6a44379f96e8d
poors <emmanuelnda77@gmail.com>
2023-03-08 12:04:38 +0000

    SaveEqualityForAll_080323_01

--- src/main/java/guru/springframework/Dollar.java
+++ src/main/java/guru/springframework/Dollar.java
@@ -1,5 +1,7 @@
 package guru.springframework;
 
+import java.util.Objects;
+
 public class Dollar {
 
     int amount;
@@ -9,5 +11,14 @@ public class Dollar {
 
     Dollar times(int multiplier) {
         return new Dollar(amount * multiplier);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        /*if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;*/
+        Dollar dollar = (Dollar) o;
+        return amount == dollar.amount;
+        //return true;
     }
 }
